f2c6605db015de82fcd47564f3f25026d12b0e33
gafifran <65336888+gafifran@users.noreply.github.com>
2020-06-29 15:30:35 -0500

    Initial commit

---
+++ EXAMEN/EXAMEN/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using EXAMEN.Pages;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace EXAMEN
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            f1.Content = new p1();
+        }
+
+        private void b1_Click(object sender, RoutedEventArgs e)
+        {
+            f1.Content = new p1();
+        }
+
+        private void b2_Click(object sender, RoutedEventArgs e)
+        {
+            f1.Content = new p2();
+        }
+
+        private void b3_Click(object sender, RoutedEventArgs e)
+        {
+            f1.Content = new p3();
+        }
+
+        private void b4_Click(object sender, RoutedEventArgs e)
+        {
+            f1.Content = new p4();
+        }
+
+        private void b5_Click(object sender, RoutedEventArgs e)
+        {
+            f1.Content = new p5();
+        }
+    }
+}
---
+++ EXAMEN/EXAMEN/Pages/p2.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace EXAMEN.Pages
+{
+    /// <summary>
+    /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class p2 : Page
+    {
+        public p2()
+        {
+            this.InitializeComponent();
+        }
+
+        private void bp2_Click(object sender, RoutedEventArgs e)
+        {
+            frame3.Content = new p2ejemplo();
+        }
+
+        private void f0_Click(object sender, RoutedEventArgs e)
+        {
+            frame3.Content = new p2_0();
+        }
+
+        private void f1_Click(object sender, RoutedEventArgs e)
+        {
+            frame3.Content = new p2_1();
+        }
+
+        private void f2_Click(object sender, RoutedEventArgs e)
+        {
+            frame3.Content = new p2_2();
+        }
+
+        private void f3_Click(object sender, RoutedEventArgs e)
+        {
+            frame3.Content = new p2_3();
+        }
+
+        private void f4_Click(object sender, RoutedEventArgs e)
+        {
+            frame3.Content = new p2_4();
+        }
+
+        private void f5_Click(object sender, RoutedEventArgs e)
+        {
+            frame3.Content = new p2_5();
+        }
+    }
+}
